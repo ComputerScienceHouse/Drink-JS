9cb5bda4f1b9572ededfcbf56062f2343f7851dc
Sean McGary <sean.mcgary@gmail.com>
2012-09-03 01:56:50 -0700

    update readme

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
 server "drink.csh.rit.edu", :app, :web, :db, :primary => true
 set :deploy_to, "/home/drink/drink_server"
 set :branch, 'master'
+
+after "deploy", "production:deps", "production:configs", "production:stop", "production:start"
